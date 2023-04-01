74c81c0a15ac849500fe00746c78589ed1d95417
nxn_404 <nafeurrahmanadil@gmail.com>
2023-04-01 22:37:58 +0600

    Add quote section and fetched data from the API

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Calculator from './components/Calculator';
 import './index.css';
+import Quote from './components/Quote';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Calculator />
+    <Quote />
   </React.StrictMode>,
 );
